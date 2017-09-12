6c21984eb75fea8835dbdd44e47d4b5dd4fe5bc9
James Hatton <james6754@hotmail.co.uk>
2017-09-12 23:10:01 +0100

    Changes to url

--- WebApplication5/Program.cs
+++ WebApplication5/Program.cs
@@ -19,7 +19,7 @@ namespace WebApplication5
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
+                .UseStartup<Startup>().UseUrls("http://127.0.0.1:5000")
                 .Build();
     }
 }
