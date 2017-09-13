6ea74e2229659b14d50e5707f05c6d883bc5f2d2
James Hatton <james6754@hotmail.co.uk>
2017-09-13 09:17:16 +0100

    nn

--- WebApplication5/Pages/About.cshtml.cs
+++ WebApplication5/Pages/About.cshtml.cs
@@ -12,7 +12,7 @@ namespace WebApplication5.Pages
 
         public void OnGet()
         {
-            bool stop = true;
+            bool stop = true;  
             Message = "Your application description page.";
         }
     }
