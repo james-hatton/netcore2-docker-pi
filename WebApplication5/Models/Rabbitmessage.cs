f5685387e059006e81fb0a1fffe24c7e9c056290
James Hatton <james6754@hotmail.co.uk>
2017-09-19 20:25:54 +0100

    Addedd rabbitmq

---
+++ WebApplication5/Models/Rabbitmessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5.Models
+{
+    public class Rabbitmessage
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+    }
+}
--- WebApplication5/Pages/About.cshtml.cs
+++ WebApplication5/Pages/About.cshtml.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RabbitMQ.Client;
+using System.Text;
+using Newtonsoft.Json;
+using WebApplication5.Models;
 
 namespace WebApplication5.Pages
 {
@@ -12,8 +16,27 @@ namespace WebApplication5.Pages
 
         public void OnGet()
         {
-            bool stop = true;  
-            Message = "Your application description page.";   
+            bool stop = true;
+            Message = "Your application description page.";
+
+
+            ConnectionFactory conn = new ConnectionFactory();
+            conn.HostName = "hattononline.co.uk";
+            conn.UserName = "guest";
+            conn.Password = "guest";
+
+            var created = conn.CreateConnection();
+
+            var channel = created.CreateModel();
+
+
+            Message = JsonConvert.SerializeObject(new Rabbitmessage { FirstName = "James", SecondName = "Hatton" });
+            channel.QueueDeclare("myqueue", false, false, false, null);
+            channel.QueueBind("myqueue", "amq.direct", "a", null);
+            for (int i = 0; i < 20; i++)
+            {
+                channel.BasicPublish("amq.direct", "a", false, null, Encoding.UTF8.GetBytes(Message));
+            }
         }
     }
 }
