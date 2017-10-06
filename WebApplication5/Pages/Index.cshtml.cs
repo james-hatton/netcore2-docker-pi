58f754c54f66912cbf41c1ad601e3859617ce6ca
James Hatton <james6754@hotmail.co.uk>
2017-10-06 19:55:49 +0100

    mounted sys and added gpio library

--- WebApplication5/Pages/Index.cshtml.cs
+++ WebApplication5/Pages/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebApplication5.Hubs;
 using Microsoft.AspNetCore.SignalR;
+using Raspberry.IO.GeneralPurpose;
 
 namespace WebApplication5.Pages
 {
@@ -19,6 +20,19 @@ namespace WebApplication5.Pages
         }
         public async void OnGet()
         {
+
+            var led1 = ConnectorPin.P1Pin11.Output();
+
+            var connection = new GpioConnection(led1);
+
+            for (var i = 0; i < 100; i++)
+            {
+                connection.Toggle(led1);
+                System.Threading.Thread.Sleep(250);
+            }
+
+            connection.Close();
+
             await hubContext.Clients.All.InvokeAsync("send", HttpContext.Request.Headers["HTTP_X_REAL_IP"].FirstOrDefault());
         }
 
